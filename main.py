12c425dffb23fc2d08d5ecf90234138d8b1fe073
Dmitry <122685309+Dragoner595@users.noreply.github.com>
2024-11-08 12:26:04 +0000

    new file:   main.py 	new file:   video_load.py
    commit

---
+++ main.py
@@ -0,0 +1,7 @@
+echo "# November" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Dragoner595/November.git
+git push -u origin main
---
+++ video_load.py
@@ -0,0 +1,40 @@
+import tqdm
+import random
+import pathlib
+import itertools
+import collections
+
+import os
+import cv2
+import numpy as np
+import remotezip as rz
+
+import tensorflow as tf
+
+from IPython import display
+from urllib import request
+from tensorflow_docs.vis import embed
+
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Only show warnings and errors
+
+URL = 'https://storage.googleapis.com/thumos14_files/UCF101_videos.zip'
+
+def list_files_from_zip_url(zip_url):
+  """ List the files in each class of the dataset given a URL with the zip file.
+
+    Args:
+      zip_url: A URL from which the files can be extracted from.
+
+    Returns:
+      List of files in each of the classes.
+  """
+  files = []
+  with rz.RemoteZip(zip_url) as zip:
+    for zip_info in zip.infolist():
+      files.append(zip_info.filename)
+  return files
+
+files = list_files_from_zip_url(URL)
+files = [f for f in files if f.endswith('.avi')]
+files[:10]
